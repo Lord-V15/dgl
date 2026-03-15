a5393ea326b5f399b89b4b4b44fc33d2beb0d030
Lord-V15 <vibhanshg@gmail.com>
2026-03-15 12:19:11 +1100

    ingore extra env vars

--- backend/app/config.py
+++ backend/app/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
 
     CORS_ORIGINS: list[str] = ["http://localhost:5173"]
 
-    model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
+    model_config = {"env_file": ".env", "env_file_encoding": "utf-8", "extra": "ignore"}
 
 
 settings = Settings()
